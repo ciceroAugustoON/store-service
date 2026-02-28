fd799f01efe12712b7e8be25e7039d6ec24e3438
ciceroAugustoON <augustocicero831@gmail.com>
2026-02-28 17:08:34 -0300

    feature: user insert resource

--- src/main/java/com/learning/store_service/resources/UserResource.java
+++ src/main/java/com/learning/store_service/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.learning.store_service.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.learning.store_service.entities.User;
 import com.learning.store_service.services.UserService;
@@ -27,5 +31,12 @@ public class UserResource {
 	@GetMapping("/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		return ResponseEntity.ok(service.findById(id));
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User user) {
+		user = service.insert(user);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(location).body(user);
 	}
 }
